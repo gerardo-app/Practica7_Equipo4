3259ffc45f1ffae965acb0166a6ad734a6d15bc5
gerardo-app <84480810+gerardo-app@users.noreply.github.com>
2021-08-12 11:00:25 -0500

    Create IOSXRv.py
    Se agrego el archivo principal Python para lanzar las configuraciones al dispositivo remoto IOS-XR en la plataforma DEVNET.

---
+++ IOSXRv.py
@@ -0,0 +1,14 @@
+from logging import config
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko.tasks import netmiko_send_config, netmiko_send_command
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+S1=nr.run(netmiko_send_config, config_file="IOSXRv.txt")
+S2=nr.run(netmiko_send_config, config_commands=["interface lo 8","description Norinir con VsCODE","ipv4 address 8.8.8.8 255.255.255.255","commit"])
+S3=nr.run(netmiko_send_command, command_string="do show ip int brief")
+print_result(S1)
+print_result(S2)
+print_result(S3)
